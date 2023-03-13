00ce95f1342dc53ea8b8a1ccc3c6d5194e239c00
Manuel Wathall <manuel.wathall@tui.com>
2023-03-13 23:46:50 +0100

    checkstyle

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public final class HelloController {
 
     @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+    public String sayHello(final @RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
 }
--- src/main/java/com/example/demo/controller/PingController.java
+++ src/main/java/com/example/demo/controller/PingController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PingController {
+public final class PingController {
     @GetMapping("/ping")
     public ResponseEntity<String> getPing() {
         return ResponseEntity.ok("pong");
